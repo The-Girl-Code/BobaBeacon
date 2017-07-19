714460c0fbcbb9bf690ffc9522e48887ddddbf98
AC-apps <AC-apps@users.noreply.github.com>
2017-07-19 11:38:31 -0700

    added project

---
+++ BobaBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BobaBeacon
+//
+//  Created by Kodo on 7/19/17.
+//  Copyright © 2017 The Girl Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
