59957e0a38432f2e34a1d561946f65e1de216d82
AC-apps <AC-apps@users.noreply.github.com>
2017-07-28 16:01:10 -0700

    Worked on recommendation, crashing stage

--- BobaBeacon/FlavorsViewController.swift
+++ BobaBeacon/FlavorsViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class FlavorsViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,3 +34,53 @@ class FlavorsViewController: UIViewController {
     */
 
 }
+
+extension FlavorsViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FlavorsImageView", for: indexPath) as! FlavorsImageView
+        
+        let memeImages: [UIImage] = [
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!,
+            UIImage(named: "boba-white")!]
+        
+        let image = memeImages[indexPath.row]
+        cell.flavorImage.image = image
+        
+        return cell
+    }
+    
+}
+
+extension FlavorsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let columns: CGFloat = 3
+        let spacing: CGFloat = 1.5
+        let totalHorizontalSpacing = (columns - 1) * spacing
+        
+        let itemWidth = (collectionView.bounds.width - totalHorizontalSpacing) / columns
+        let itemSize = CGSize(width: itemWidth, height: itemWidth)
+        
+        return itemSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.5
+    }
+    
+    
+}
+
