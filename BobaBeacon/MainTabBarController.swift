b2497ba5b7b170bed4638850a82f0a25c7fdf848
Ananya Bhat <cookeymonstr@gmail.com>
2017-08-04 15:31:36 -0700

    Changed tab bar images

--- BobaBeacon/MainTabBarController.swift
+++ BobaBeacon/MainTabBarController.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+extension UIImage {
+    func makeImageWithColorAndSize(color: UIColor, size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
+
 class MainTabBarController: UITabBarController {
     
     let photoHelper = PostViewController()
@@ -22,11 +33,20 @@ class MainTabBarController: UITabBarController {
         
         delegate = self as! UITabBarControllerDelegate
         
-        tabBar.unselectedItemTintColor = .black
-        // Do any additional setup after loading the view.
+        self.tabBar.unselectedItemTintColor = UIColor(red:0.87, green:0.85, blue:1.00, alpha:1.0)        // Do any additional setup after loading the view.
     }
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+        // Sets the default color of the icon of the selected UITabBarItem and Title
+        self.tabBar.tintColor = UIColor(red:0.82, green:0.90, blue:0.67, alpha:1.0)
+        
+        // Sets the default color of the background of the UITabBar
+      //  UITabBar.appearance().barTintColor = UIColor.white
+        
+        // Sets the background color of the selected UITabBarItem (using and plain colored UIImage with the width = 1/5 of the tabBar (if you have 5 items) and the height of the tabBar)
+//UITabBar.appearance().selectionIndicatorImage = UIImage().makeImageWithColorAndSize(color: UIColor.white, size: CGSize(width: tabBar.frame.width/5, height: tabBar.frame.height))
+        
+    }
     
     /*
      // MARK: - Navigation
